8542e4c4e61f2f89cd1579c49a4d6c8a424ea0bc
kwonhoe1121 <kwonhoe1121@gmail.com>
2020-01-27 19:43:09 +0900

    add basic structure of dvdRentMngSysPjt

---
+++ dvdRentMngSysPjt/src/screenOut.h
@@ -0,0 +1,18 @@
+/* Name: screenOut.h  ver 1.0
+ * Content: 콘솔 출력을 위한 함수들의 선언.
+ * Implementation: KJJ
+ * 
+ * Last modified 2020/01/27
+ */
+
+#ifndef __SCREENOUT_H__
+#define __SCREENOUT_H__
+
+#include "cusInfo.h"
+
+void ShowMenu(void);
+void ShowCustomerInfo(cusInfo * pCus);
+
+#endif
+
+/* end of file */
--- make-pjt.sh
+++ make-pjt.sh
@@ -4,9 +4,9 @@ TARGET=$PWD/$1
 
 mkdir -p $TARGET/src $TARGET/file $TARGET/test
 
-cp $PWD/dbg/dbg.h $TARGET/src
+cp $PWD/dbg/dbg.h $PWD/dbg/common.h $TARGET/src
 cp $PWD/dbg/Makefile $TARGET
-
+cp run.sh $TARGET
 
 
 
